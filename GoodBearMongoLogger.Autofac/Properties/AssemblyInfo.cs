4948f5f19d29193e32b381519a84c971235408a6
Ian <ian_carey_936@hotmail.com>
2016-11-29 13:46:26 +0000

    Assembly file updates

--- GoodBearMongoLogger.Autofac/Properties/AssemblyInfo.cs
+++ GoodBearMongoLogger.Autofac/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GoodBearMongoLogger.Autofac")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GoodBear Technology")]
 [assembly: AssemblyProduct("GoodBearMongoLogger.Autofac")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
